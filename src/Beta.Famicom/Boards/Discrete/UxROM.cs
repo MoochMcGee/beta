2c3414165efee87049736f98137e945073224cc1
Adam Becker <beannaich@gmail.com>
2016-08-07 20:42:44 -0600

    [FC] Adding `UxROM` support.

---
+++ src/Beta.Famicom/Boards/Discrete/UxROM.cs
@@ -0,0 +1,70 @@
+﻿using Beta.Famicom.Formats;
+using Beta.Platform.Messaging;
+
+namespace Beta.Famicom.Boards.Discrete
+{
+    [BoardName("U(.*)ROM")]
+    public sealed class UxROM : IBoard
+    {
+        private readonly CartridgeImage image;
+
+        private int prg_page;
+
+        public UxROM(CartridgeImage image)
+        {
+            this.image = image;
+        }
+
+        public void R2A03Read(ushort address, ref byte data)
+        {
+            if ((address & 0x8000) == 0x8000)
+            {
+                this.image.prg.Read(MapPrgAddress(address), ref data);
+            }
+        }
+
+        public void R2A03Write(ushort address, byte data)
+        {
+            if ((address & 0x8000) == 0x8000)
+            {
+                prg_page = data;
+            }
+        }
+
+        private int MapPrgAddress(ushort address)
+        {
+            return (address & 0xc000) == 0x8000
+                ? (address & 0x3fff) | (prg_page << 14)
+                : (address & 0x3fff) | (~0 << 14)
+                ;
+        }
+
+        public void R2C02Read(ushort address, ref byte data)
+        {
+            if ((address & 0x2000) == 0x0000)
+            {
+                image.chr.Read(address, ref data);
+            }
+        }
+
+        public void R2C02Write(ushort address, byte data)
+        {
+            if ((address & 0x2000) == 0x0000)
+            {
+                image.chr.Write(address, data);
+            }
+        }
+
+        public bool VRAM(ushort address, out int a10)
+        {
+            var x = (address >> 10) & image.h;
+            var y = (address >> 11) & image.v;
+
+            a10 = x | y;
+
+            return true;
+        }
+
+        public void Consume(ClockSignal e) { }
+    }
+}
